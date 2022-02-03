430891a78644bd9e57e7472db6354b4782aff83c
iagardi <garbigz@gmail.com>
2022-02-03 15:19:31 +0000

    vanity test

--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -71,7 +71,8 @@ public class MarsRoverShould {
 	}
 
 	@ParameterizedTest
-	@CsvSource({"LLM, 0:9:S",})
+	@CsvSource({"LLM, 0:9:S",
+			  "LLMMMMMMMMMM, 0:0:S"})
 	void turn_left_twice__and_move_south(String command, String expected) {
 		var game = new MarsRover();
 		var result = game.getNexPosition(command);
