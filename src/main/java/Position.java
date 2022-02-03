3663ba41ea71b830bcd5ef772028a6ce4c77f0ff
sofia <sofia.carballo@codurance.com>
2022-02-03 16:11:55 +0100

    green

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -11,6 +11,9 @@ public class Position {
 		if (direction.getDirection().equals("E")) {
 			xAxis = xAxis < 9 ? xAxis + 1 : 0;
 		}
+		if (direction.getDirection().equals("W")) {
+			xAxis = xAxis == 0 ? 9 : xAxis - 1;
+		}
 		if (direction.getDirection().equals("N")) {
 			yAxis = yAxis < 9 ? yAxis + 1 : 0;
 		}
