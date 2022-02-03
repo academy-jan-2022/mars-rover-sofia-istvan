cb29d52c531a38b829f502ee9bf067266a15a0e6
iagardi <garbigz@gmail.com>
2022-02-03 10:30:58 +0000

    red test

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,5 +1,9 @@
 public class MarsRover {
 	public String move(String movement) {
+		if (movement.equals("RR")) {
+			return "0:0:S";
+		}
+
 		return "0:0:E";
 	}
 }
--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MarsRoverShould {
 
 	@Test void
-	return_00E_when_entering_one_R(){
+	return_00E_when_turning_right_once(){
 		var game = new MarsRover();
 		var result = game.move("R");
 
@@ -13,10 +13,20 @@ public class MarsRoverShould {
 	}
 
 	@Test void
-	return_00S_when_entering_two_R(){
+	return_00S_when_turning_right_twice(){
 		var game = new MarsRover();
 		var result = game.move("RR");
 
 		assertEquals("0:0:S", result);
 	}
+
+	@Test void
+	return_00W_when_turning_right_three_times(){
+		var game = new MarsRover();
+		var result = game.move("RRR");
+
+		assertEquals("0:0:W", result);
+	}
+
+
 }
