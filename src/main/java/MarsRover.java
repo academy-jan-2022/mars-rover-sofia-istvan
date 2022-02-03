edc3a79b6d3eb916c24fc2e730b33a5fff7b96bd
iagardi <garbigz@gmail.com>
2022-02-03 10:21:59 +0000

    red test

---
+++ src/main/java/MarsRover.java
@@ -0,0 +1,5 @@
+public class MarsRover {
+	public String move(String movement) {
+		return "";
+	}
+}
---
+++ src/test/java/MarsRoverShould.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MarsRoverShould {
+
+	@Test void
+	return_00E_when_entering_R(){
+		var game = new MarsRover();
+		var result = game.move("R");
+
+		assertEquals("0:0:E", result);
+
+	}
+
+}
