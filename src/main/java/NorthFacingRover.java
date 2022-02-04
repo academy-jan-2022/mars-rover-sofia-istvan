4dfe9a5c4f31793aa699ebfdc7c99f91967fef97
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-04 12:45:41 +0100

    refactor

--- src/main/java/NorthFacingRover.java
+++ src/main/java/NorthFacingRover.java
@@ -2,17 +2,19 @@ public class NorthFacingRover extends MarsRover2 {
 	@Override
 	protected MarsRover2 executeSingle(String singleCommand) {
 		if(singleCommand.equals("M")) {
-			if (y == 9) {
-				y = 0;
-			} else {
-				y++;
-			}
+			y = getNextIndex(y);
 			return this;
 		}
 		if (singleCommand.equals("R")) {
 			return new EastFacingRover();
 		}
 		return new WestFacingRover();
+	}
+
+	private int getNextIndex(int currentValue) {
+		if (currentValue == 9)
+			return 0;
+		return currentValue + 1;
 	}
 
 	@Override
