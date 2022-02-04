0c2421928c7a7bca56d8e6f2fa84673596a46037
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-04 16:59:28 +0100

    vanity

--- src/test/java/ControllerShould.java
+++ src/test/java/ControllerShould.java
@@ -11,4 +11,13 @@ public class ControllerShould {
 
        assertEquals("0:0:E", result);
     }
+
+    @Test
+    void
+    return_south_facing_position(){
+        var controller = new Controller(new CartesianGrid());
+        var result = controller.execute("RRRL");
+
+        assertEquals("0:0:S", result);
+    }
 }
