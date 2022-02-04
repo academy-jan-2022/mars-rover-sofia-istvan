cc596607e0b99d5fdb1d9072f233253c1cb411e0
Brian Hoang <brian.hd.hoang@codurance.com>
2022-02-04 11:29:47 +0000

    green

--- src/main/java/WestFacingRover.java
+++ src/main/java/WestFacingRover.java
@@ -1,7 +1,7 @@
 public class WestFacingRover extends MarsRover2 {
 	@Override
 	protected MarsRover2 executeSingle(String singleCommand) {
-		return null;
+		return new SouthFacingRover();
 	}
 
 	@Override
