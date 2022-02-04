3ce0cf1eba8b712a8aa8ff15095ecfbcfd24a8a5
sofia <sofia.carballo@codurance.com>
2022-02-04 11:43:32 +0100

    green

--- src/main/java/NorthFacingRover.java
+++ src/main/java/NorthFacingRover.java
@@ -1,6 +1,9 @@
 public class NorthFacingRover implements MarsRover2 {
 	@Override
 	public MarsRover2 execute(String command) {
+		if(command.equals("M")) {
+			return this;
+		}
 		if (command.equals("R"))
 			return new EastFacingRover();
 		return new WestFacingRover();
