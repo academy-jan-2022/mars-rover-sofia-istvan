f9a62a2c2b5398a9473f6dd9dbb954319a546d2a
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-04 12:25:22 +0100

    green

--- src/main/java/SouthFacingRover.java
+++ src/main/java/SouthFacingRover.java
@@ -1,7 +1,8 @@
 public class SouthFacingRover extends MarsRover2 {
 	@Override
 	protected MarsRover2 executeSingle(String singleCommand) {
-		return null;
+
+		return new EastFacingRover();
 	}
 
 	@Override
