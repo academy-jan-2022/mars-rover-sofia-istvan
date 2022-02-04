cebbce8f6980fcd549db55d40454aa77563265c3
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-04 12:28:36 +0100

    red

---
+++ src/test/java/WestFacingRoverShould.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import org.junit.jupiter.api.Test;
+
+public class WestFacingRoverShould {
+	@Test void
+	head_south_when_instructed_to_turn_left() {
+		MarsRover2 rover = new WestFacingRover();
+		var newRover = rover.execute("L");
+		assertInstanceOf(SouthFacingRover.class, newRover);
+	}
+}
