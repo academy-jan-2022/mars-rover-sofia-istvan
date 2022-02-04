e5767d76694a5119459d9066b0ea4e27dbc64d45
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-04 12:26:26 +0100

    red

--- src/test/java/SouthFacingRoverShould.java
+++ src/test/java/SouthFacingRoverShould.java
@@ -8,4 +8,11 @@ public class SouthFacingRoverShould {
 		var newRover = rover.execute("L");
 		assertInstanceOf(EastFacingRover.class, newRover);
 	}
+
+	@Test void
+	head_west_when_instructed_to_turn_right(){
+		MarsRover2 rover = new SouthFacingRover();
+		var newRover = rover.execute("R");
+		assertInstanceOf(WestFacingRover.class, newRover);
+	}
 }
