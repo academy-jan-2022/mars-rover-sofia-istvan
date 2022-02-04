f1073c0ad9e008013b14d274aab32b0d0880db4c
danihra-codurance <daniel.rodriguez@codurance.com>
2022-02-04 15:42:46 +0100

    red

--- src/test/java/CartesianGridShould.java
+++ src/test/java/CartesianGridShould.java
@@ -8,4 +8,10 @@ public class CartesianGridShould {
 		var nextGrid = grid.moveRight();
 		assertEquals(1, nextGrid.getX());
 	}
+	@Test void
+	move_two_times_right_within_limits() {
+		var grid = new CartesianGrid(10, 10);
+		var nextGrid = grid.moveRight().moveRight();
+		assertEquals(2, nextGrid.getX());
+	}
 }
