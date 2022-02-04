683ccdd7ff9bd487d62f57c9913a4e3ee24488c7
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-04 12:31:52 +0100

    green

--- src/main/java/WestFacingRover.java
+++ src/main/java/WestFacingRover.java
@@ -1,6 +1,8 @@
 public class WestFacingRover extends MarsRover2 {
 	@Override
 	protected MarsRover2 executeSingle(String singleCommand) {
+		if (singleCommand.equals("R"))
+			return new NorthFacingRover();
 		return new SouthFacingRover();
 	}
 
