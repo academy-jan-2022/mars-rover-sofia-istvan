867b71ab74b557392fa1a0197c8b3d990e3576cd
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-07 10:26:05 +0100

    refactor

--- src/main/java/CartesianGrid.java
+++ src/main/java/CartesianGrid.java
@@ -1,6 +1,6 @@
 public class CartesianGrid {
 	private final GridBoundaries boundaries;
-	private final Point point;
+	private final Point roverPosition;
 
 	public CartesianGrid() {
 		this(new GridBoundaries(10, 10));
@@ -10,36 +10,36 @@ public class CartesianGrid {
 		this(boundaries, new Point());
 	}
 
-	private CartesianGrid(GridBoundaries boundaries, Point point) {
+	private CartesianGrid(GridBoundaries boundaries, Point roverPosition) {
 		this.boundaries = boundaries;
-		this.point = point;
+		this.roverPosition = roverPosition;
 	}
 
 	public CartesianGrid moveRight() {
-		var newX = boundaries.wrapOnWidth(point.x() + 1);
-		return new CartesianGrid(boundaries, new Point(newX, point.y()));
+		var newX = boundaries.wrapOnWidth(roverPosition.x() + 1);
+		return new CartesianGrid(boundaries, new Point(newX, roverPosition.y()));
 	}
 
 	public int getX() {
-		return point.x();
+		return roverPosition.x();
 	}
 
 	public CartesianGrid moveLeft() {
-		var newX = boundaries.wrapOnWidth(point.x() - 1);
-		return new CartesianGrid(boundaries,new Point(newX, point.y()));
+		var newX = boundaries.wrapOnWidth(roverPosition.x() - 1);
+		return new CartesianGrid(boundaries,new Point(newX, roverPosition.y()));
 	}
 
 	public CartesianGrid moveTop() {
-		var newY = boundaries.wrapOnHeight(point.y() + 1);
-		return new CartesianGrid(boundaries, new Point(point.x(), newY));
+		var newY = boundaries.wrapOnHeight(roverPosition.y() + 1);
+		return new CartesianGrid(boundaries, new Point(roverPosition.x(), newY));
 	}
 
 	public int getY() {
-		return point.y();
+		return roverPosition.y();
 	}
 
 	public CartesianGrid moveBottom() {
-		var newY = boundaries.wrapOnHeight(point.y() - 1);
-		return new CartesianGrid(boundaries, new Point(point.x(), newY));
+		var newY = boundaries.wrapOnHeight(roverPosition.y() - 1);
+		return new CartesianGrid(boundaries, new Point(roverPosition.x(), newY));
 	}
 }
